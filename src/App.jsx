da21bc0ec67890406e8ca45b18cf6018db80b854
adnan <azizart320480@gmail.com>
2024-02-29 23:34:46 +0500

    some

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
        <ToastContainer />
-      <RoutesComponents />
-      {/* <NewAddToCart/> */}
+      {/* <RoutesComponents /> */}
+      <NewAddToCart/>
     </>
   );
 }
